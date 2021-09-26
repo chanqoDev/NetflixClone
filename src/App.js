f98a4314e911f0ad4ab96725491135bac71e9384
Christian E <christianelena92@gmail.com>
2021-09-25 23:21:08 -0500

    Fetching data with React Hooks and display fetch images.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+//import the row, file dir
+import Row from "./Row"; // this should pull in the compo we created
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1> Hey christian lets test that is running netflix clone locally. </h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrendings} />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,39 @@
+// We type this template constantly because we are often building components.
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+// https://image.tmdb.org/t/p/original/8j12tohv1NBZNmpU93f47sAKBbw.jpg
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]); // This is called a hook, and we are init an empty arr
+
+  //  A snippet of code which runs based on a specific condition
+  //  when this row compotent loads, I want to make a request to TDB api and be able to pull the information when the row loads, the image and from those speicific catagories
+  //  if [], run once when the row loads, and dont run it again
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  // the reason we want them in a container is so we can scroll through them.
+  console.log(movies);
+  /* container -> posters "films" */
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {/* several row__poster(s) */}
+        {movies.map((movie) => (
+          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
