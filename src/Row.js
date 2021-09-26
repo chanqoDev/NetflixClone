76b01ad296bfa5425625dd87650854b4783cc75e
Christian E <christianelena92@gmail.com>
2021-09-26 01:05:18 -0500

    get all the movies and build the rows completed

--- src/Row.js
+++ src/Row.js
@@ -5,7 +5,7 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 // https://image.tmdb.org/t/p/original/8j12tohv1NBZNmpU93f47sAKBbw.jpg
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]); // This is called a hook, and we are init an empty arr
 
   //  A snippet of code which runs based on a specific condition
@@ -32,8 +32,10 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
