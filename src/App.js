75bebf1ae79323f5cf8fc26f52adf8a3033780ba
Christian E <christianelena92@gmail.com>
2021-09-27 17:18:18 -0500

    Complete NavBar, logo and effects

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-      {/* nav */}
+      <Nav />
       <Banner />
-
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
+        isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
